e7ff70b6d5115897b332bdf897c3d1ae2e491d48
alexandra <alexandraschurman@gmail.com>
2022-01-14 11:53:49 -0800

    commit tree 605c90c7544b5d8e91956553942cf34999de2cbf

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function superbowlWin(record, object) {
+    for (let object of record){
+        object[result] === "W"
+    }
+    return object[result]
+}
 
+console.log(superbowlWin())
+
+// returned false & undefined??
+// function superbowlWin(record) {
+//     return record.result === "W";
+
+// }
+
+// console.log(record.find(superbowlWin));
+
+// function superbowlWin() {
+//     for (let result of record) {
+//         result === "W";
+//     }
+//     console.log(record.year)
+// }
+
+// const superbowlWin = record.find( ({ result }) => result === "W");
+
+// console.log(superbowlWin())
